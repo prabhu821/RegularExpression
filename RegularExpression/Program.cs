77c844ffc50bd47a483386a0004a319ba6f2e5fc
rishiraj007 <rishiraj@mounttalent.com>
2023-06-21 15:56:57 +0530

    [Prabhu]Add: validate first name

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Regular Expression!");
+            Pattern pattern = new Pattern();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter option to choose \n1.First Name \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("\nEnter First Name");
+                        string fName = Console.ReadLine();
+                        pattern.validateFirstName(fName);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
